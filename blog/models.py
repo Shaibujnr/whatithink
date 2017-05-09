de1bc9a942cbea218bc2a6c9e0b849981974e52e
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2017-05-09 22:25:07 +0100

    defined the model classes

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=20, default="New Post Title")
+    content = models.TextField(default="New Post Content")
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    message = models.TextField(default="New Post Comment")

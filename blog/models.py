059b6053bac204c65ecf0c966ee6427d5a5c7126
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2017-05-13 12:54:57 +0100

    implemented str method for models

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,13 @@ class Post(models.Model):
     title = models.CharField(max_length=20, default="New Post Title")
     content = models.TextField(default="New Post Content")
 
+    def __str__(self):
+        return "%s"%self.title
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post)
     message = models.TextField(default="New Post Comment")
+
+    def __str__(self):
+        return "%s"%self.message[:15]+"..."

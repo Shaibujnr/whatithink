76e213ca17218661120b6dfc72879090fad91b97
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2017-05-13 13:16:45 +0100

    included local host to allow hosts and testing env var

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
+
 
 # Create your views here.
+
+def home(request):
+    if request.method == 'GET':
+        test_var = os.environ["MY_TEST_VARIABLE"] if "MY_TEST_VARIABLE" in os.environ else "Development mode"
+        http = "Welcome to HOME page and the test variable is %s" % test_var
+        return HttpResponse(http, status=200)

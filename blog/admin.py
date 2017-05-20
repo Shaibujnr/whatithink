d61aaa646344b2fe6b09b563b2099ce5114d9462
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2017-05-20 17:57:28 +0100

    customized the admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from whatithink.adminsite import myAdminSite
 from .models import Post,Comment
 from redactor.widgets import RedactorEditor
-from django.db import models
-from django.forms import Textarea,ModelForm
+from django.forms import ModelForm
+
+
+
 
 # Register your models here.
 class PostModelForm(ModelForm):
@@ -17,7 +20,6 @@ class PostModelAdmin(admin.ModelAdmin):
     form = PostModelForm
 
 
+myAdminSite.register(Post,PostModelAdmin)
+myAdminSite.register(Comment)
 
-admin.site.register(Post,PostModelAdmin)
-
-admin.site.register(Comment)
--- whatithink/urls.py
+++ whatithink/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from .adminsite import myAdminSite
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
+    url(r'^admin/',myAdminSite.urls),
     url(r'^', include('blog.urls')),
     url(r'^redactor/', include('redactor.urls')),
     url(

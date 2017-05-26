deb129c4a6a07381b1400ca5c9b859f196cb0f84
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2017-05-26 17:13:06 +0100

    using media url for image src

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import os
 from bs4 import BeautifulSoup
 from PIL import Image
-
+from whatithink.settings import MEDIA_URL
 MEDIA_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "media/uploads")
 
 
@@ -14,22 +14,12 @@ class Post(models.Model):
 
     @property
     def get_preview_image(self):
-        def resize_image(img,size,img_name):
-            im2 = img.resize(size,Image.ANTIALIAS)
-            im2.save(img_name,quality=90)
-
         soup = BeautifulSoup(self.content,'html.parser')
         summary_image = soup.find(id='si')
         if not summary_image:
-            return "/media/defaults/preview.png"
+            return MEDIA_URL+"defaults/preview.png"
         image_source = summary_image['src']
-        image_file = os.path.join(MEDIA_PATH,image_source.split('/')[-1])
-        im1 = Image.open(image_file)
-        w,h = im1.size
-        pw,ph = 700,300
-        if w != pw and h != ph:
-            resize_image(im1,(pw,ph),image_file)
-        return image_source if summary_image else "/media/defaults/preview.png"
+        return image_source if summary_image else MEDIA_URL+"defaults/preview.png"
 
     @property
     def get_summary(self):
